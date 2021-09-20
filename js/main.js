59507a7e7b09c4a405ea4103d9ddb933b2517e67
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2021-09-20 12:15:55 +0300

    finish

---
+++ js/main.js
@@ -0,0 +1,79 @@
+$(function(){
+
+    $(".rate-yo").rateYo({
+        rating: 3.6,
+        multiColor: {
+          "startColor": "#00B67A", 
+          "endColor"  : "#00B67A" 
+        }
+      });
+      $(".slider-star").rateYo({
+        rating: 5,
+        starWidth: "20px",
+        multiColor: {
+          "startColor": "#ffffff", 
+          "endColor"  : "#ffffff" 
+        }
+      });
+
+      $(".slider__inner").slick({
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1230,
+            settings: {
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 820,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false
+            }
+          }
+        ]
+      });
+      $(".guided-slider").slick({
+        centerMode: true,
+        slidesToShow: 4,
+        slidesToScroll:1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1379,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll:1,
+            }
+          },
+          {
+            breakpoint: 1050,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll:1,
+            }
+          },
+          {
+            breakpoint: 580,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll:1,
+            }
+          }
+          
+        ]
+      });
+
+
+      $('.health-btn').on('click', function(){
+        $('.health-btn').removeClass('health-btn--active');
+        $(this).toggleClass('health-btn--active');
+      });
+      
+});
+
